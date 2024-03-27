f6283267927183aae651ceb02b39ee84db252ab7
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-28 00:16:18 +0530

    #Day57

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
-const BASE_URL= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+const BASE_URL="https://api.currencyapi.com/v3/latest?apikey=cur_live_J15b6qIgLYib5QQ0U1i1CclBGkotGkIIbFDz0Gqb";
+
+
+
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
-
-for(let select of dropdowns){
-    for(currCode in countryList){
-        let newOption =document.createElement("option");
-        newOption.innerText=currCode;
-        newOption.value=currCode;
-        select.append(newOption);
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "From" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
     }
+    select.append(newOption);
+  }
 
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 }
+
+// basically reset the -value to the positive value---
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+//  courrency value accessbility-----
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+//  basically for the flag changes at the time of courrency chnage
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
